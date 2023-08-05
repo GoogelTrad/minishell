7ebe5fba578022220f33a1530bf76de48fa66492
elisa <elisa1.pdx@gmail.com>
2023-08-05 17:51:29 +0200

    export fini + trie par ordre ascii

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: elisa <elisa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:22:52 by elisa             #+#    #+#             */
-/*   Updated: 2023/08/04 19:40:22 by elisa            ###   ########.fr       */
+/*   Updated: 2023/08/05 17:50:17 by elisa            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,11 +60,38 @@ int	add_var_env(char *word, char *value)
 	return (1);
 }
 
+void		export_by_ascii(char **tab, int size_env)
+{
+	int		order;
+	int		i;
+	char	*tmp;
+
+	order = 0;
+	while (tab && order == 0)
+	{
+		order = 1;
+		i = 0;
+		while (i < size_env - 1)
+		{
+			if (ft_strcmp(tab[i], tab[i + 1]) > 0)
+			{
+				tmp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = tmp;
+				order = 0;
+			}
+			i++;
+		}
+		size_env--;
+	}
+}
+
 void	aff_export_alone(int fd)
 {
 	char	**tab;
 
 	tab = copy_tab(g_minishell.env);
+	export_by_ascii(tab, g_minishell.size_env);
 	export_alone(fd, tab);
 }
 
@@ -90,40 +117,6 @@ int	ft_valid_arg(char *str)
 	}
 	return (1);
 }
-
-/*void    export(t_command *c)
-{
-    int k;
-    int i;
-
-    i = 0;
-    if (!c->option[0])
-        aff_export_alone(c->fd_out);
-    else
-    {
-        while (c->option[i])
-        {
-            k = 0;
-            while (c->option[i][k] && c->option[i][k] != '=')
-            {
-                if (!(ft_valid_arg(c->option[i])))
-                {
-                    printf("%s: '%s': not a valid identifier1\n",
-                    c->cmd, c->option[i]);
-                    g_minishell.status = 1;
-                    return ;
-                }
-                k++;
-            }
-            if (c->option[i][k] == '\0')
-                add_var_env(ft_strdup(c->option[i]), NULL);
-            else
-                add_var_env(get_char(c->option[i], 0, k), c->option[i] + k + 1);
-            g_minishell.status = 0;
-            i++;
-        }
-    }
-}*/
 
 void    export(t_command *c)
 {
