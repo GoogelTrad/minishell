b6ed9510f78071354088d6b23bee35ec13997056
GoogelTrad <celianmichez@free.fr>
2023-08-30 15:24:09 +0200

    code erreur command not found

--- src/utils_exec.c
+++ src/utils_exec.c
@@ -6,7 +6,7 @@
 /*   By: cmichez <cmichez@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 20:41:46 by cmichez           #+#    #+#             */
-/*   Updated: 2023/08/28 21:03:15 by cmichez          ###   ########.fr       */
+/*   Updated: 2023/08/30 15:20:07 by cmichez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	no_command(int verif, t_command *c)
 	{
 		if (!verif_slash(c->cmd))
 		{
+			g_status = 127;
 			write(2, c->cmd, ft_strlen(c->cmd));
 			write(2, ": ", 2);
 			write(1, "command not found\n", 18);
