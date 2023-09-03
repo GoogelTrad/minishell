cb4576b07b973b0efb3b714c6aab2d11f1ab298f
GoogelTrad <celianmichez@free.fr>
2023-09-03 22:58:10 +0200

    code erreur exit

--- src/builtins/utils_exit.c
+++ src/builtins/utils_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils_exit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: elisa <elisa@student.42.fr>                +#+  +:+       +#+        */
+/*   By: cmichez <cmichez@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 12:14:18 by elisa             #+#    #+#             */
-/*   Updated: 2023/08/23 12:14:59 by elisa            ###   ########.fr       */
+/*   Updated: 2023/09/03 22:57:23 by cmichez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	err_(t_command *c)
 {
 	ft_putstr("exit\n");
 	printf("minishell: exit: %s: numeric argument required\n", c->option[0]);
-	exit(255);
+	exit(2);
 }
 
 void	err_nb_arg(void)
